1c755f95197d56e678ab98b54960ed3bd2b432db
Max Davish <mdavish@yext.com>
2022-04-17 15:42:58 -0400

    adding armadillo secret variable

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 import core from "@actions/core";
 import github from "@actions/github";
 
+const armadilloUrls = {
+  'PROD': 'https://www.witharmadillo.com/',
+  'DEV': 'https://dev.witharmadillo.com/'
+}
+
 try {
   const cloudRunUrl = core.getInput("cloud-run-url");
   const modelId = core.getInput("model-id");
   const commitHash = core.getInput("commit-hash");
   const environment = core.getInput("environment")
+  const armadilloGithubSecret = core.getInput("armadillo-github-secret")
+
   console.log({
     cloudRunUrl,
     modelId,
     commitHash,
-    environment
+    environment,
+    armadilloGithubSecret,
   })
+
+  // Example Output:
+  // const sampleOutput = {
+  //   cloudRunUrl: 'https://model-7082e3d73ba5d0790a5761439de5cfa6a7c24f17-iv7xltpeca-uc.a.run.app',
+  //   modelId: 'armadillo-ai/automated-test',
+  //   commitHash: '7082e3d73ba5d0790a5761439de5cfa6a7c24f17',
+  //   environment: 'PROD'
+  //   armadilloGithubSecret: '<<REDACTED>>'
+  // }
+
+  if (!armadilloUrls.hasOwnProperty(environment)) {
+    throw new Error(`Environment ${environment} is not supported`)
+  }
+
+  const baseUrl = armadilloUrls[environment];
+  const fullUrl = `${baseUrl}`;
+
+
 } catch (error) {
   core.setFailed(error.message);
 }
